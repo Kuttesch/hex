7a13ed4ffbca38e477d76b16148515a567173f3d
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-07-07 03:37:14 +0200

    changed for github pages

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function createAndAppendDiv(container, index) {
   newDiv.dataset.value = color;
   newDiv.style.backgroundColor = color;
   newDiv.onclick = function() {
-    var url = new URL('/sub', window.location.href);
+    var url = new URL('/hex/sub', window.location.href);
     url.hash = this.dataset.value;
     window.location.href = url.toString();
   };
